37d78ba97b1ded0915f664db4c3b7227aa0054f7
unknown <dembadiack@outlook.com>
2021-11-07 22:25:57 +0000

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 2000;
+const path = require("path");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+
+
+
+
+
+app.use(cors());
+
+
+app.use(bodyParser.json());
+
+app.use(helmet.frameguard({ action: "sameorigin" }));
+
 
 if (process.env.NODE_ENV === "production") {
 
